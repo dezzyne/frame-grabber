891e26cfed11c55257a642c27f73dd0fd4ae6c4f
Matthew Lance Fuller <matthewlancefuller@gmail.com>
2022-05-21 13:10:13 -0700

    Bootstrap frame-grabber

---
+++ CODE_OF_CONDUCT.md
@@ -0,0 +1,3 @@
+# The Dezzyne Code of Conduct
+
+The Code of Conduct for this repository [can be found online](https://github.com/dezzyne/.github/blob/main/CODE_OF_CONDUCT.md).
---
+++ src/main.rs
@@ -0,0 +1,53 @@
+extern crate log;
+
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
+use log::Level;
+use path_absolutize::Absolutize;
+use vid2img::FileSource;
+
+fn main() {
+    if env::args().len() != 3 {
+        return;
+    }
+    let args: Vec<String> = env::args().collect();
+    let input_file = &args[1];
+    let output_directory = &args[2];
+
+    let input_file_as_path = Path::new(input_file);
+    if !input_file_as_path.is_file() {
+        return;
+    }
+
+    let output_directory_as_path = Path::new(output_directory);
+    if !output_directory_as_path.is_dir() {
+        return;
+    }
+
+    log::log!(
+        Level::Debug,
+        "{}\n{}",
+        input_file_as_path.absolutize().unwrap().to_str().unwrap(),
+        output_directory_as_path
+            .absolutize()
+            .unwrap()
+            .to_str()
+            .unwrap()
+    );
+
+    let input_file_as_path_as_ref = input_file_as_path.as_ref();
+
+    let video_source = FileSource::new(input_file_as_path_as_ref, (1920, 1080)).unwrap();
+
+    let mut first_frame = File::create(output_directory_as_path.join("first_image.png")).unwrap();
+
+    for (_n, frame) in video_source.into_iter().enumerate() {
+        if let Ok(Some(png_img_data)) = frame {
+            first_frame.write_all(&png_img_data).unwrap();
+            break;
+        }
+    }
+}
